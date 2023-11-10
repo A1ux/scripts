c52861be7a19eeb5eae751b628013fe03bf06e5c
A1ux <ruanocristian97@gmail.com>
2023-11-10 17:20:56 -0600

    nmap changes

--- python_scripts/genCustomCommands.py
+++ python_scripts/genCustomCommands.py
@@ -3,7 +3,7 @@ import argparse
 # Configurar los argumentos
 parser = argparse.ArgumentParser(description='Generar comandos personalizados a partir de un archivo con IPs o CIDRs.')
 parser.add_argument('-f', '--file', required=True, help='Archivo de entrada que contiene IPs o CIDRs')
-parser.add_argument('-o', '--output', required=True, help='Nombre del archivo de salida')
+parser.add_argument('-o', '--output', help='Nombre del archivo de salida (opcional)')
 parser.add_argument('-c', '--command', required=True, help='Comando personalizado con marcadores de posición IP')
 args = parser.parse_args()
 
@@ -11,9 +11,16 @@ args = parser.parse_args()
 with open(args.file, 'r') as input_file:
     ips = input_file.read().splitlines()
 
-with open(args.output, 'w') as output_file:
+# Comprobar si se ha especificado un archivo de salida
+if args.output:
+    # Escribir los comandos en el archivo de salida si se especifica
+    with open(args.output, 'w') as output_file:
+        for ip in ips:
+            nmap_command = args.command.replace('IP', ip) + '\n'
+            output_file.write(nmap_command)
+    print(f'Se han generado los comandos en {args.output}')
+else:
+    # Imprimir los comandos en pantalla si no se especifica archivo de salida
     for ip in ips:
-        nmap_command = args.command.replace('IP', ip) + '\n'
-        output_file.write(nmap_command)
-
-print(f'Se han generado los comandos en {args.output}')
+        nmap_command = args.command.replace('IP', ip)
+        print(nmap_command)
--- python_scripts/genNmapCommands.py
+++ python_scripts/genNmapCommands.py
@@ -4,7 +4,7 @@ import argparse
 # Configura los argumentos
 parser = argparse.ArgumentParser(description='Generar comandos Nmap desde un archivo JSON.')
 parser.add_argument('-f', '--file', required=True, help='Archivo JSON de entrada')
-parser.add_argument('-o', '--output', default='comandos_nmap.txt', help='Nombre del archivo de salida')
+parser.add_argument('-o', '--output', help='Nombre del archivo de salida (opcional)')
 parser.add_argument('-c', '--command', required=True, help='Comando Nmap con marcadores de posición IP y PORTS')
 args = parser.parse_args()
 
@@ -12,13 +12,16 @@ args = parser.parse_args()
 with open(args.file, 'r') as file:
     data = json.load(file)
 
-# Crear y abrir el archivo de salida
-with open(args.output, 'w') as output:
+# Comprobar si se ha especificado un archivo de salida
+if args.output:
+    # Crear y abrir el archivo de salida si se especifica
+    with open(args.output, 'w') as output:
+        for ip, ports in data.items():
+            nmap_command = args.command.replace('IP', ip).replace('PORTS', ','.join(map(str, ports))) + '\n'
+            output.write(nmap_command)
+    print(f'Se han generado los comandos en {args.output}')
+else:
+    # Imprimir en pantalla si no se especifica archivo de salida
     for ip, ports in data.items():
-        # Reemplazar los marcadores de posición IP y PORTS en el comando Nmap
-        nmap_command = args.command.replace('IP', ip).replace('PORTS', ','.join(map(str, ports))) + '\n'
-
-        # Escribir el comando en el archivo de salida
-        output.write(nmap_command)
-
-print(f'Se han generado los comandos en {args.output}')
+        nmap_command = args.command.replace('IP', ip).replace('PORTS', ','.join(map(str, ports)))
+        print(nmap_command)
